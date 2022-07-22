4e51e8aa7ffa9c6d62f2956c38cf9910eec3c382
ShubhangiGoel24 <75439736+ShubhangiGoel24@users.noreply.github.com>
2022-07-22 23:26:23 +0530

    Add files via upload

---
+++ DS I/Day 1/217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length<=1){
+            return false;
+        }
+        for(int i=1;i<nums.length;i++){
+            if(nums[0]==nums[i]){
+                return true;
+            }
+            int arr[]=new int[]
+        }
+        
+    }
+}
